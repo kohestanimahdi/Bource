c1cf6d41b320848e72bfd51d338c6d59855ab427
Mahdi Koohestani <koohestani@atighgasht.ir>
2021-07-28 21:55:00 +0430

    add swagger

---
+++ Bource.WebConfiguration/Configuration/Swagger/SwaggerConfigurationExtensions.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Bource.WebConfiguration.Configuration.Swagger
+{
+    public static class SwaggerConfigurationExtensions
+    {
+        public static void AddSwagger(this IServiceCollection services, string applicationTitle)
+        {
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = applicationTitle ?? Assembly.GetEntryAssembly().GetName().Name,
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Mahdi Kohestani",
+                        Email = "kohestanimahdi@gmail.com",
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "kohestanimahdi.ir",
+                        Url = new Uri("https://kohestanimahdi.ir/")
+                    }
+                });
+                //For Include comments of each actions in swagger
+
+                var xmlFile = Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetEntryAssembly().GetName().Name}.xml");
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+
+                options.OperationFilter<RemoveVersionParameters>();
+
+                options.DocumentFilter<SetVersionInPaths>();
+
+                options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Scheme = "bearer"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearer" }
+                        },
+                        new string[]{}
+                    }
+                });
+
+            });
+
+            services.AddSwaggerGenNewtonsoftSupport();
+        }
+
+        public static void UseSwaggerAndUI(this IApplicationBuilder app, string prefix = null)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1");
+                c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.List);
+                c.RoutePrefix = prefix ?? "docs";
+            });
+
+            //ReDoc UI middleware. ReDoc UI is an alternative to swagger-ui
+            app.UseReDoc(options =>
+            {
+                options.SpecUrl("/swagger/v1/swagger.json");
+
+                options.EnableUntrustedSpec();
+                options.ScrollYOffset(10);
+                options.HideHostname();
+                options.HideDownloadButton();
+                options.ExpandResponses("200,201");
+                options.RequiredPropsFirst();
+                options.NoAutoAuth();
+                options.PathInMiddlePanel();
+                options.HideLoading();
+                options.NativeScrollbars();
+                options.DisableSearch();
+                options.OnlyRequiredInSamples();
+                options.SortPropsAlphabetically();
+
+            });
+        }
+    }
+}
